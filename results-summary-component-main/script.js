e7904d854bf7e2c99a73b7d2603b25505e1fec65
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-05-06 01:25:02 -0300

    better css + js added
    better css positioning and sizing @midia and js added to use data.json

---
+++ results-summary-component-main/script.js
@@ -0,0 +1,22 @@
+const $finalResult = document.querySelector('.final-result')
+const $scoreRankRange = document.querySelector('.score-rank-range')
+
+const $summary = document.querySelectorAll('.categories .points')
+
+async function logJSONData() {
+
+  const response = await fetch('data.json')
+  const jsonData = response.json()
+
+  jsonData.then((data) => {
+    let med = 0
+    for (let i = 0; i < $summary.length; i++) {
+      $summary[i].textContent = data[i].score
+      med += data[i].score
+    }
+    $finalResult.textContent = Math.floor(med/$summary.length)
+  })
+  
+}
+
+logJSONData()

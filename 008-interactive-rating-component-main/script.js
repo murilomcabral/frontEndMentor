8e097f194a17423f077bb8f4db1abefb6cababe8
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-05-18 20:16:29 -0300

    desktop design done

--- 008-interactive-rating-component-main/script.js
+++ 008-interactive-rating-component-main/script.js
@@ -1,14 +1,33 @@
-const fbValueList = document.querySelectorAll('.fb-value li')
-const arr = Array.from(fbValueList)
+const $fbValueList = document.querySelectorAll('.fb-value li')
+
+const $rateState = document.querySelector('.rate-state')
+const $thankYouState = document.querySelector('.thank-you-state')
+
+const $showRated = document.querySelector('.rated')
+
+const arr = Array.from($fbValueList)
 console.log(arr)
 
-let points =
-  arr.forEach((e) => {
-    e.addEventListener('click', (event) => {
+let pointsRated = 0
+
+arr.forEach((e) => {
+  e.addEventListener('click', (event) => {
+    if (event.target.hasAttribute('checked')) {
+      event.target.removeAttribute('checked')
+    } else {
       arr.forEach((elem) => elem.removeAttribute('checked'))
       event.target.setAttribute('checked','')
-      console.log(event.target.innerText)
-      return event.target.innerText
-    })
-  });
+      pointsRated = event.target.innerText
+    }
+  })
+});
 
+function thankYouState() {
+  arr.forEach((e) => {
+    if (e.hasAttribute('checked')) {
+      $rateState.style.display = 'none'
+      $thankYouState.style.display = 'grid'
+      $showRated.textContent = pointsRated
+    }
+  })
+}

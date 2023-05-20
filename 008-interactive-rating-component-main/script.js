56166449c6c6fbe0a35fe2611d893d0710342d8a
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-05-19 23:36:55 -0300

    main added

--- 008-interactive-rating-component-main/script.js
+++ 008-interactive-rating-component-main/script.js
@@ -24,19 +24,28 @@ arr.forEach((e) => {
 function thankYouState() {
   arr.forEach((e) => {
     if (e.hasAttribute('checked')) {
-      $rateState.style.display = 'none'
-      $thankYouState.style.display = 'grid'
+      $rateState.style.cssText = `
+      display: none;
+      visibility: hidden;
+      `
+      $thankYouState.removeAttribute('style')
       $showRated.textContent = pointsRated
     }
   })
+  animate()
 }
 
 // svg animation
 const linePath = document.querySelector('.svg-line path')
 const pathLen = linePath.getTotalLength()
-linePath.style.cssText = `
-fill:none;
-stroke:#e6e6e6;
-stroke-width:1.1px;
-stroke-dasharray:${pathLen / 100}, 5.5, 0, 0;
-`
+
+async function animate() {
+  linePath.style.cssText = `
+  fill:none;
+  stroke:#e6e6e6;
+  stroke-width:1.1px;
+  stroke-dasharray:${pathLen / 100}, 5.5, 0, 0;
+  animation: dashOffset 7s infinite linear;
+  `
+  // animation: dashOffset ${5 / pointsRated + 1}s infinite linear;
+}

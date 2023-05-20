7fe46ac4bf818f8d177bdbf1af914e869b1f5cfc
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2023-05-20 01:04:36 -0300

    converting to form

--- 008-interactive-rating-component-main/script.js
+++ 008-interactive-rating-component-main/script.js
@@ -1,44 +1,33 @@
-const $fbValueList = document.querySelectorAll('.fb-value li')
-const arr = Array.from($fbValueList)
+function validate() {
+  let valid = false
+  const $form = document.rateForm.rating
+  let vote = ''
 
-const $rateState = document.querySelector('.rate-state')
-const $thankYouState = document.querySelector('.thank-you-state')
-
-const $showRated = document.querySelector('.rated')
-
-
-let pointsRated = 0
-
-arr.forEach((e) => {
-  e.addEventListener('click', (event) => {
-    if (event.target.hasAttribute('checked')) {
-      event.target.removeAttribute('checked')
-    } else {
-      arr.forEach((elem) => elem.removeAttribute('checked'))
-      event.target.setAttribute('checked','')
-      pointsRated = event.target.innerText
+  for(let radio of $form) {
+    if(radio.checked) {
+      vote = radio.value
+      valid = true
+      break
     }
-  })
-});
+  }
 
-function thankYouState() {
-  arr.forEach((e) => {
-    if (e.hasAttribute('checked')) {
-      $rateState.style.cssText = `
-      display: none;
-      visibility: hidden;
-      `
-      $thankYouState.removeAttribute('style')
-      $showRated.textContent = pointsRated
-    }
-  })
-  animate()
+  if(valid) {
+    showFinal()
+  } else {
+    alert('You need to select ONE option first!')
+  }
+}
+
+function showFinal() {
+  const $thankYouState = document.querySelector('.thank-you-state')
+  const $rateState = document.querySelector('.rate-state')
+  $thankYouState.removeAttribute('style')
+  $rateState.style.cssText = 'display:none;visibility:hidden;'
 }
 
 // svg animation
 const linePath = document.querySelector('.svg-line path')
 const pathLen = linePath.getTotalLength()
-
 async function animate() {
   linePath.style.cssText = `
   fill:none;
@@ -47,5 +36,4 @@ async function animate() {
   stroke-dasharray:${pathLen / 100}, 5.5, 0, 0;
   animation: dashOffset 7s infinite linear;
   `
-  // animation: dashOffset ${5 / pointsRated + 1}s infinite linear;
 }
